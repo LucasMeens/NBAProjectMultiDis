334a0741298950b137e95167b555ff101c989953
LucasMeens <lucas.meens@edu.esiee.fr>
2025-12-31 16:29:25 +0100

    Adding first version of clean_data.py (not complete)

---
+++ NBAProjectMultiDis/src/utils/clean_data.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+# Cleaning and writing the "cities.csv" file for population density
+
+us_cities = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/uscities.csv")
+canada_cities = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/canadacities.csv")
+
+us_select = us_cities[["city", "density", "lat", "lng"]]
+canada_select = canada_cities[["city", "density", "lat", "lng"]]
+
+concatenation = pd.concat([us_select, canada_select], ignore_index=True)
+
+concatenation_unique = (
+    concatenation
+    .assign(city=concatenation["city"].str.strip().str.title()) # Preventing duplicate caused by merging different datasets
+    .sort_values("density", ascending=False)
+    .drop_duplicates(subset="city")
+)
+
+concatenation_unique.to_csv("NBAProjectMultiDis/data/cleaned/cities.csv")
+
+# Cleaning and writing the "franchises.csv" file for NBA franchises location
+
+franchises = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/franchise_locations.csv")
+
+NBA = franchises[franchises["League"] == "NBA"]
+selection = NBA[["Team", "Lat", "Long"]]
+
+selection = selection.rename(columns={
+    "Team":"franchise",
+    "Lat": "lat",
+    "Long": "lng"
+})
+
+selection.to_csv("NBAProjectMultiDis/data/cleaned/franchises.csv")
+
+# Cleaning and writing the "wins.csv" file for NBA finals, winners, mvp, etc..
+
+wins = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/NBA Finals and MVP.csv")
+
+wins = wins.rename(columns={
+    "Year": "year",
+    "Western Champion": "west_champion",
+    "Eastern Champion": "east_champion",
+    "Result": "result",
+    "NBA Champion": "champion",
+    "MVP Name": "mvp",
+    "MVP Team": "mvp_team",
+})
+
+wins_selected = wins[["year", "west_champion", "east_champion", "result", "champion", "mvp", "mvp_team"]]
+
+wins_selected.to_csv("NBAProjectMultiDis/data/cleaned/wins.csv")
+
+# Cleaning and writing the "games.csv" file for our graphics on points averages by year
+21800549
+
+games = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/games.csv")
+
+games = games.rename(columns={
+    "hometeamCity": "home_city",
+    "hometeamName": "home_name",
+    "awayteamCity": "away_city",
+    "awayteamName": "away_name",
+    "homeScore": "home_score",
+    "awayScore": "away_score",
+})
+
+games_selected = games[["home_city", "home_name", "away_city", "away_name", "home_score", "away_score", "winner"]]
+
+games_selected.to_csv("NBAProjectMultiDis/data/cleaned/games.csv")
+
+# NOT WORKING FOR NOW, PROBLEM WITH THE WINNER BEING AN ID
