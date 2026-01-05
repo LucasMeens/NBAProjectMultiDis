126346176ad1085b82d37bb9b6acdd9d89f2bb0a
LucasMeens <lucas.meens@edu.esiee.fr>
2026-01-05 09:43:10 +0100

    Refactoring clean_players_data and including it in clean_data.py

--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 # Cleaning and writing the "cities.csv" file for population density
 
-us_cities = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/uscities.csv")
-canada_cities = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/canadacities.csv")
+us_cities = pd.read_csv("data/raw/csvs/uscities.csv")
+canada_cities = pd.read_csv("data/raw/csvs/canadacities.csv")
 
 us_select = us_cities[["city", "density", "lat", "lng"]]
 canada_select = canada_cities[["city", "density", "lat", "lng"]]
@@ -17,11 +17,11 @@ concatenation_unique = (
     .drop_duplicates(subset="city")
 )
 
-concatenation_unique.to_csv("NBAProjectMultiDis/data/cleaned/cities.csv", index=False)
+concatenation_unique.to_csv("data/cleaned/cities.csv", index=False)
 
 # Cleaning and writing the "franchises.csv" file for NBA franchises location
 
-franchises = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/franchise_locations.csv")
+franchises = pd.read_csv("data/raw/csvs/franchise_locations.csv")
 
 NBA = franchises[franchises["League"] == "NBA"]
 selection = NBA[["Team", "Lat", "Long"]]
@@ -32,11 +32,11 @@ selection = selection.rename(columns={
     "Long": "lng"
 })
 
-selection.to_csv("NBAProjectMultiDis/data/cleaned/franchises.csv", index=False)
+selection.to_csv("data/cleaned/franchises.csv", index=False)
 
 # Cleaning and writing the "wins.csv" file for NBA finals, winners, mvp, etc..
 
-wins = pd.read_csv("NBAProjectMultiDis/data/raw/csvs/NBA Finals and MVP.csv")
+wins = pd.read_csv("data/raw/csvs/NBA Finals and MVP.csv")
 
 wins = wins.rename(columns={
     "Year": "year",
@@ -50,13 +50,13 @@ wins = wins.rename(columns={
 
 wins_selected = wins[["year", "west_champion", "east_champion", "result", "champion", "mvp", "mvp_team"]]
 
-wins_selected.to_csv("NBAProjectMultiDis/data/cleaned/wins.csv", index=False)
+wins_selected.to_csv("data/cleaned/wins.csv", index=False)
 
 # Cleaning and writing the "games.csv" file for our graphics on points averages by year
 # 21800549
 
 games = pd.read_csv(
-    "NBAProjectMultiDis/data/raw/csvs/games.csv",
+    "data/raw/csvs/games.csv",
     usecols=[
         "hometeamCity",
         "hometeamName",
@@ -91,4 +91,32 @@ games["winner_name"] = games.apply(get_winner, axis=1)
 
 games_selected = games[["home_city", "home_name", "away_city", "away_name", "home_score", "away_score", "winner_name"]]
 
-games_selected.to_csv("NBAProjectMultiDis/data/cleaned/games.csv", index=False)
+games_selected.to_csv("data/cleaned/games.csv", index=False)
+
+# Cleaning and writing 'players.csv' file for player's stat
+
+players_stats = pd.read_csv("data/raw/csvs/nba_player_stats.csv")
+
+players_selected = players_stats[[
+    "season", "player", "pos", "age", 
+    "team_id", "g", "mp_per_g", "pts_per_g",
+    "ast_per_g", "trb_per_g", "stl_per_g", "blk_per_g",
+    "fg_pct", "fg3_pct", "ft_pct"
+]]
+
+players_selected = players_selected.rename(columns={
+    "g" : "games",
+    "mp_per_g" : "min_per_game",
+    "pts_per_g" : "points_per_game",
+    "ast_per_g" : "assists_per_game",
+    "trb_per_g" : "rebounds_per_game",
+    "stl_per_g" : "steals_per_game",
+    "blk_per_g" : "blocks_per_game",
+    "fg_pct" : "bucket2_percentage",
+    "fg3_pct" : "bucket3_percentage",
+    "ft_pct" : "total_percentage",
+})
+
+players_selected.to_csv("data/cleaned/players_stats.csv", index=False)
+
+
