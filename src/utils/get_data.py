511f6d7126eafcd8d98517f30583ef7ef00b83b6
LucasMeens <lucas.meens@edu.esiee.fr>
2026-01-15 10:20:45 +0100

    Downloading of all kaggles csv

--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -15,3 +15,23 @@ games = Path(path_g) / "Games.csv"
 dest_g = Path("data/raw/csvs/games.csv")
 # No need of checking parent here because we did it earlier
 shutil.copy(games, dest_g)  
+
+# Downloading schedule2026.csv
+schedule = Path(path_g) / "LeagueSchedule25_26.csv"
+dest_s = Path("data/raw/csvs/schedule26.csv")
+# No need of checking parent here because we did it earlier
+shutil.copy(schedule, dest_s)
+
+# Downloading franchise_locations.csv
+path_fl = kg.dataset_download("logandonaldson/sports-stadium-locations")
+locations = Path(path_fl) / "stadiums.csv"
+dest_fl = Path("data/raw/csvs/franchise_locations.csv")
+# No need of checking parent here because we did it earlier
+shutil.copy(locations, dest_fl)
+
+# Downloading nba_player_stats.csv
+path_pl = kg.dataset_download("robertsunderhaft/nba-player-season-statistics-with-mvp-win-share")
+stats = Path(path_pl) / "NBA_Dataset.csv"
+dest_ps = Path("data/raw/csvs/nba_player_stats.csv")
+# No need of checking parent here because we did it earlier
+shutil.copy(stats, dest_s)
