27b1ab43f614d649c610f7d203f2ca36f1fa9eb5
LucasMeens <lucas.meens@edu.esiee.fr>
2026-01-24 15:15:04 +0100

    Creating the filter comp

--- src/components/containers/dashboard_view.py
+++ src/components/containers/dashboard_view.py
@@ -5,26 +5,15 @@ from src.stats_service import *
 from src.components.components.header import header
 from src.components.components.charts import charts_block
 from src.components.components.team_cards import team_cards_component
-from src.components.components.filter import filter
+from src.components.components.filter import *
 from src.stats_service import get_team_list, get_season_list
 
 def dashboard_layout():
-    teams = get_team_list()
-    seasons = get_season_list()
-    options_teams = [{"label": "🏀 Toutes les franchises", "value": "ALL"}] + [{"label": t, "value": t} for t in teams]
-
+    teams, seasons, options_teams = get_variables()
 
     return html.Div([
         header(),
-        
-        html.Div([
-                html.Label("Franchise", style={"fontWeight": "bold", "color": "#1f2937"}),
-                dcc.Dropdown(id="team-dropdown", options=options_teams, value="ALL", clearable=False, style={"marginBottom": "15px"}),
-                html.Label("Saison", style={"fontWeight": "bold", "color": "#1f2937"}),
-                dcc.Dropdown(id="season-dropdown", options=[{"label": str(s), "value": s} for s in seasons], value=seasons[0]),
-                html.Label("Options Carte", style={"marginTop": "15px", "fontWeight": "bold", "display": "block"}),
-                dcc.Checklist(id="map-options", options=[{"label": " Afficher densité population", "value": "density"}], value=[], style={"padding": "10px 0"}),
-        ], style={"width": "28%", "display": "inline-block", "verticalAlign": "top", "padding": "20px", "backgroundColor": "white", "borderRadius": "12px", "boxShadow": "0 4px 6px -1px rgba(0,0,0,0.1)"}),
+        filter(seasons, options_teams),
 
         html.Div([charts_block()], style={"padding": "0 20px"}),
 
