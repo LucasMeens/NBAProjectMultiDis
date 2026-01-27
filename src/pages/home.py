5b29e027dc9aefb61cf1364821a1358d2f12bc8a
LucasMeens <lucas.meens@edu.esiee.fr>
2026-01-27 18:09:16 +0100

    Home page CSS and function done (Finnally !)

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,15 +1,60 @@
 import dash
-from dash import html
+from dash import html, dcc
 from src.components.components.header import header
 
 dash.register_page(__name__, path='/')
 
 def layout():
     return html.Div(
-    [
-        header(),
+        [
+            html.Div(
+                className="background-image"
+            ),
 
-        html.H1('Home page'),
-        html.P('In progress...')
-    ]
-)
+            html.Div( 
+                [
+                    html.Div(
+                        [ 
+                            html.Div(
+                                "PROJET MULTIDISCIPLINAIRE S1 2025-2026", 
+                                className="project-box"
+                            ),
+
+                            html.H1(
+                                "ANALYSE DES FRANCHISES NBA",
+                                className="home-title"
+                            ),
+
+                            html.P(
+                                "Une immersion dans les statistiques de la NBA. Densité de population, performance des joueurs et statistiques des franchises.", 
+                                className="home-subtitle"
+                            ),
+
+                            html.Div(
+                                [
+                                    dcc.Link(
+                                        "ALLER AUX GRAPHIQUES",
+                                        href="/charts",
+                                        className="home-button"
+                                    ),
+                                    dcc.Link(
+                                        "RECHERCHER UN JOUEUR",
+                                        href="/players",
+                                        className="home-button"
+                                    ),
+                                    dcc.Link(
+                                        "OÙ SONT LES FRANCHISES ?",
+                                        href="/map",
+                                        className="home-button",
+                                    ),
+                                ],
+                                className="links-container"
+                            )
+                        ],
+                        className="home-page"
+                    )
+                ],
+                className="blue-wall"
+            ),
+        ]
+    )
